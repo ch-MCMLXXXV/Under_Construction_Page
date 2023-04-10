6a935144a759c56c801bc8c06e285e4fff58d3fe
Courtney Harris <cjharris1985@gmail.com>
2023-04-10 14:53:20 -0500

    changed image

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,15 @@ export default function Home() {
             />
             <link rel='icon' href='/favicon.ico' />
          </Head>
-         <Image src='/under.jpg' fill alt='Under Construction' />
+         <section className=' bg-candle bg-cover h-screen bg-no-repeat bg-center'>
+            <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
+               <div className='mr-auto place-self-center lg:col-span-7'>
+                  <h1 className='max-w-2xl mb-4 text-4xl text-white font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl'>
+                     Under Construction
+                  </h1>
+               </div>
+            </div>
+         </section>
       </>
    );
 }
